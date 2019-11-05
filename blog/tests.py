152b9a897934780fd57c9e845525e1a8f3986730
ben <bvanbever1@gmail.com>
2019-11-05 11:47:02 +0100

    initial commit

---
+++ blog/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+
+from django.test import Client, TestCase
+
+from django.urls import reverse
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+        self.post = Post.objects.create(
+            title='A good title',
+            body='Nice body content',
+            author=self.user,
+        )
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
